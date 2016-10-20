f14032f4d39d11894276c05a556224d81f40fe5b
talum <tracy.a.lum@gmail.com>
2016-10-19 21:47:18 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Code React element here
+const title = React.createElement('h1', {}, 'An Awesome Person');
+const paragraph = React.createElement('p', {}, 'Who is learning React');
+
+const listItems = [
+  React.createElement('li', {}, 'JavaScript'),
+  React.createElement('li', {}, 'React'),
+  React.createElement('li', {}, 'Movies'),
+  React.createElement('li', {}, 'Ice cream')
+]
+
+const list = React.createElement('ul', {}, listItems);
+const meInReact = React.createElement('div', {className: 'me'}, [title, paragraph, list]);
+ReactDOM.render(
+  meInReact,
+  document.getElementById('main')
+);
